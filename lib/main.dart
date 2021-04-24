787cb92677eb54668eacb66ac648c1682707a2d6
me-aditya <adityarathi1212@gmail.com>
2021-04-25 00:23:18 +0530

    Basics of row and column.

--- lib/main.dart
+++ lib/main.dart
@@ -14,28 +14,26 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("First Flutter App"),
       ),
-      body: Center(
-        child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.teal,
-              gradient: LinearGradient(colors: [Colors.red, Colors.pink]),
-              boxShadow: [
-                BoxShadow(
-                    blurRadius: 10,
-                    color: Colors.grey[800],
-                    offset: Offset(5.0, 2.0))
-              ],
-            ),
-            height: 200,
-            width: 200,
-            alignment: Alignment.center,
-            child: Text("First Flutter App",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  
-                ))),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.red,
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.green,
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.pink,
+          ),
+        ],
       ),
     );
   }
