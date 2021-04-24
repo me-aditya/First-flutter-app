72a74e01f42da0fdac7c7c1bbb800ced3b64449e
me-aditya <adityarathi1212@gmail.com>
2021-04-25 00:00:11 +0530

    Container and text decoration

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,28 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("First Flutter App"),
       ),
-      body: Container(
-        child: Center(child: Text("Hello Flutter")),
+      body: Center(
+        child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [Colors.red, Colors.pink]),
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 10,
+                    color: Colors.grey[800],
+                    offset: Offset(5.0, 2.0))
+              ],
+            ),
+            height: 200,
+            width: 200,
+            alignment: Alignment.center,
+            child: Text("First Flutter App",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  
+                ))),
       ),
     );
   }
