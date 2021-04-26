dc50699667194d47aadca0741be561a512a2aca9
me-aditya <adityarathi1212@gmail.com>
2021-04-26 09:57:47 +0530

    Made new login page card component so stack code will look clearer and cleaner.

---
+++ lib/login_page_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class LoginPageCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: Form(
+                  child: Column(children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Email",
+                    labelText: "Email",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                    border: OutlineInputBorder(),
+                  ),
+                )
+              ])),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+            child: Text("Login"),
+            onPressed: () {},
+          ),
+        ]),
+      ),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first/login_page_card.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -14,45 +15,6 @@ class _LoginPageState extends State<LoginPage> {
         appBar: AppBar(
           title: Text("Login Screen"),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Column(children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Card(
-                  child: Form(
-                      child: Column(children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "Enter Email",
-                        labelText: "Email",
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: "Enter Password",
-                        labelText: "Password",
-                        border: OutlineInputBorder(),
-                      ),
-                    )
-                  ])),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                child: Text("Login"),
-                onPressed: () {},
-              ),
-            ]),
-          ),
-        ));
+        body: LoginPageCard());
   }
 }
